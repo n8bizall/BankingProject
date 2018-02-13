a5ab97a908f41ff3dc567e4044475298b1d7089b
Nate Bucher <n8bizall@yahoo.com>
2018-02-13 12:04:26 -0500

    Everything is working here

---
+++ BankingProject/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject
+{
+    public class Account
+    {
+     //properties
+            public int Id { get; set; }
+            public Customer Owner { get; set; }
+            public string Description { get; set; }
+            public double Balance { get; set; }
+    //methods
+            public void Deposit(double amount)
+        {
+           if(!IsAmountNegative(amount))
+            {
+            
+                Balance = Balance + amount;
+            }
+            //if (amount < 0)                                                                    I created a new method that calls this action so I dont duplicate code
+            //{
+            //    Console.WriteLine("Amount cannot be negative");
+            //    return;
+            //}
+                                                                    //assignment statement --single equalsign is an assignment 
+        }
+            public void Withdraw(double amount)
+        {
+            IsAmountNegative(amount);
+
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insuffucient Funds");
+                return;
+            }
+            if (!IsAmountNegative(amount))
+            {
+
+                Balance = Balance + amount;
+            }
+            
+            
+        }
+            public double GetBalance()
+        {
+            return Balance;
+        }
+        private bool IsAmountNegative(double amount)
+        {
+             if (amount < 0)
+            {
+                Console.WriteLine("Amount cannot be negative");
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace BankingProject
 {
-    class Program
+    class Program                                                           //properties and methdods are contained in classes 
     {
-        static void Main(string[] args)
+        static void Main(string[] args)                                     // operating enviroment looks for this method to start running the program
         {
+            
+            Account acct1 = new Account();                                  // using the Account type unboxing the Account type--when using class as type use this format
+            acct1.Id = 1;
+            acct1.Description = "My first checking account!!";
+            acct1.Balance = 0.0;
+
+            acct1.Deposit(5.00);
+            Console.WriteLine("The balance is " + acct1.GetBalance() + " should be five dollars");
+
+            acct1.Deposit(-5);
+            Console.WriteLine("The balance is " + acct1.GetBalance() + " should be zero");
+            
+
+
+            acct1.Withdraw(5000.00);
+            Console.WriteLine("The balance is " + acct1.GetBalance() + " should be -4995");
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
